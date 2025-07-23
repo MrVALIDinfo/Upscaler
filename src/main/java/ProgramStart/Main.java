a8ea3c32c36a3ee2d37d3a9959630175d429b658
MrVALIDinfo <eldiyar@eldiyars-MacBook-Air.local>
2025-07-23 18:18:25 +0600

    test

---
+++ src/main/java/ProgramStart/Main.java
@@ -0,0 +1,9 @@
+package ProgramStart;
+
+import ui.GeneralWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        new GeneralWindow();
+    }
+}
--- src/main/java/ui/GeneralWindow.java
+++ src/main/java/ui/GeneralWindow.java
@@ -1,5 +1,17 @@
 package ui;
 
-public class GeneralWindow {
+import javax.swing.*;
+import java.awt.*;
 
+public class GeneralWindow extends JFrame {
+
+    public GeneralWindow() {
+        setTitle("My Swing Window");
+        setSize(900, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        setVisible(true);
+    }
 }
